720395c5d1f0b09873d6406362679027f3b458dd
Phạm Tiến Thành <tienthanh.dqc@gmail.com>
2017-04-15 18:42:04 +0700

    Update
    Cập nhật tên file

--- get.php
+++ get.php
@@ -64,9 +64,6 @@ if(!isset($m[1]))
 
 $title = $m[1];
 
-$old_shell = is_file('zingtv.sh') ? file_get_contents('zingtv.sh') : '';
-file_put_contents('zingtv.sh', $old_shell . 'aria2c -x8 --out="' . $title . '.mp4" ' . $downloadLink . PHP_EOL);
-
 // ----------------------------------------------
 $m = [];
 
@@ -87,6 +84,9 @@ if(!empty($m[1]) && !empty($m[2]))
 		exit('Error, API return empty data');
 	}
 
+	preg_match_all('/itemprop="name" content="(.*?)"/i', $m['html'], $m2);
+	//print_r($m2);
+
 	preg_match_all('/href="(.*?)" title/i', $m['html'], $m);
 	//print_r($m);
 	//exit;
@@ -97,7 +97,9 @@ if(!empty($m[1]) && !empty($m[2]))
 	foreach($m[1] as $v)
 	{
 		if($v == $_url)
+		{
 			break;
+		}
 
 		$found++;
 	}
@@ -106,9 +108,14 @@ if(!empty($m[1]) && !empty($m[2]))
 	//echo $found;
 	if(!empty($m[1][$found]))
 	{
+		$title = !empty($m2[1][$found-1]) ? $m2[1][$found-1] : $title;
 		$nextUrl = 'http://tv.zing.vn' . $m[1][$found];
 	}
 }
+
+$old_shell = is_file('zingtv.sh') ? file_get_contents('zingtv.sh') : '';
+file_put_contents('zingtv.sh', $old_shell . 'aria2c -x8 --out="' . $title . '.mp4" ' . $downloadLink . PHP_EOL);
+
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -129,6 +136,10 @@ if(!empty($m[1]) && !empty($m[2]))
 		Tìm thấy tập tiếp theo, tự chuyển hướng sau 1s...<br>
 		Tập tiếp theo: <?php echo htmlspecialchars($nextUrl); ?>...<br>
 		Nếu không tự chuyển vui lòng bấm vào <a href="get.php?next=on&url=<?php echo urlencode($nextUrl); ?>">đây</a>.
+		
+		<pre>
+		<?php echo print_r($m2[1], true); ?>
+		</pre>
 		
 		<script>
 			setTimeout(function(){
